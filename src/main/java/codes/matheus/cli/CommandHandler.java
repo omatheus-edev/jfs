a1fa2f77bebf6d3c39b83619ab33134a5a981d0c
omatheus-edev <mc2019750@gmail.com>
2026-01-30 20:39:00 -0300

    improved code

--- src/main/java/codes/matheus/cli/CommandHandler.java
+++ src/main/java/codes/matheus/cli/CommandHandler.java
@@ -3,21 +3,28 @@ package codes.matheus.cli;
 import codes.matheus.core.BuildTree;
 import codes.matheus.core.Core;
 import codes.matheus.core.FileOperations;
+import codes.matheus.util.Colors;
 import org.jetbrains.annotations.NotNull;
 
 public final class CommandHandler {
     private final @NotNull FileOperations operations;
-    private final @NotNull Core core;
 
     public CommandHandler(@NotNull Core core, @NotNull BuildTree build) {
-        this.core = core;
         this.operations = new FileOperations(core, build);
     }
 
     public void execute(@NotNull String input) {
-        @NotNull Command command = Command.create(input);
-        if (command.getType().equals(Command.Type.UNKNOWN)) return;
+        if (input.isBlank()) return;
 
-        operations.execute(command);
+        try {
+            @NotNull Command command = Command.create(input);
+            if (command.getType().equals(Command.Type.UNKNOWN)) {
+                return;
+            }
+
+            operations.execute(command);
+        } catch (Exception e) {
+            System.out.println(Colors.format("Error in execute command: " + e.getMessage(), Colors.RED));
+        }
     }
 }
